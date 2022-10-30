5cb7892524da4338b71a949b6dede1e8788028db
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-10-30 19:49:37 +0900

    react導入

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from "@material-ui/core";
 
 function Example() {
     return (
@@ -7,11 +8,14 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">うい</div>
 
                         <div className="card-body">
                             I'm an example component!
                         </div>
+                        <Button color="primary" variant="contained">
+                            Hello World
+                        </Button>
                     </div>
                 </div>
             </div>
