6976c07d57d0f07761b569ebd26dde64d00234ac
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-11-02 08:44:18 +0900

    feat: mySqlからデータを取得する関数を作成

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Button, Card } from "@material-ui/core";
 
+import axios from "axios"; //追記する
+
 function Home() {
+    // const [posts, setPosts] = useState([]);
+
+    axios
+        .get("/api/posts")
+        .then((response) => {
+            // setPosts(response.data); //バックエンドから返ってきたデータでpostsを更新する
+            console.log(response.data); //取得データ確認用のconsole.log()
+        })
+        .catch(() => {
+            console.log("通信に失敗しました");
+        });
+
     return (
         <div className="container">
             <Card>
